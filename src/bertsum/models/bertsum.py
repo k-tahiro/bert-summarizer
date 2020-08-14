e7cba334eeac595c4d709593cd808801b865906f
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-08-14 20:54:32 +0900

    Refactor

--- src/bertsum/models/bertsum.py
+++ src/bertsum/models/bertsum.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from transformers import AutoModel, AutoTokenizer
 import torch
@@ -8,8 +8,12 @@ from torch import nn
 class BertSumExt(nn.Module):
     def __init__(self,
                  model_type: str,
-                 num_encoder_heads: int = 8,
-                 num_encoder_layers: int = 2):
+                 nhead: int = 8,
+                 dim_feedforward: int = 2048,
+                 dropout: float = 0.1,
+                 activation: str = 'relu',
+                 num_layers: int = 2,
+                 norm: nn.Module = None):
         super(BertSumExt, self).__init__()
 
         # sentence embedding layer
@@ -17,16 +21,20 @@ class BertSumExt(nn.Module):
 
         # inter-sentence contextual embedding layer
         encoder_layer = nn.TransformerEncoderLayer(d_model=self.bert.config.hidden_size,
-                                                   nhead=num_encoder_heads)
+                                                   nhead=nhead,
+                                                   dim_feedforward=dim_feedforward,
+                                                   dropout=dropout,
+                                                   activation=activation)
         self.encoder = nn.TransformerEncoder(encoder_layer,
-                                             num_layers=num_encoder_layers)
+                                             num_layers=num_layers,
+                                             norm=norm)
 
         # classifier layer
         self.linear = nn.Linear(self.bert.config.hidden_size, 1)
 
     def forward(self,
                 src: torch.Tensor,
-                cls_idxs: list = None,
+                cls_idxs: List[int] = None,
                 bert_args: Dict[str] = {},
                 encoder_args: Dict[str] = {}):
         if cls_idxs is None:
@@ -46,25 +54,37 @@ class BertSumExt(nn.Module):
 class BertSumAbs(nn.Module):
     def __init__(self,
                  model_type: str,
-                 num_decoder_heads: int = 8,
-                 num_decoder_layers: int = 6):
+                 nhead: int = 8,
+                 dim_feedforward: int = 2048,
+                 dropout: float = 0.2,
+                 activation: str = 'gelu',
+                 num_layers: int = 6,
+                 norm: nn.Module = None,
+                 eps: float = 1e-6):
         super(BertSumAbs, self).__init__()
 
         # encoder
         self.encoder = AutoModel.from_pretrained(model_type)
+        hidden_size = self.encoder.config.hidden_size
 
         # decoder
         tokenizer = AutoTokenizer.from_pretrained(model_type)
         pad_token = tokenizer.special_tokens_map_extended['pad_token']
         padding_idx = tokenizer.vocab[pad_token]
         self.embeddings = nn.Embedding(self.encoder.config.vocab_size,
-                                       self.encoder.config.hidden_size,
+                                       hidden_size,
                                        padding_idx=padding_idx)
 
-        decoder_layer = nn.TransformerDecoderLayer(d_model=self.encoder.config.hidden_size,
-                                                   nhead=num_decoder_heads)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size,
+                                                   nhead=nhead,
+                                                   dim_feedforward=dim_feedforward,
+                                                   dropout=dropout,
+                                                   activation=activation)
+        if norm is None and eps:
+            norm = nn.LayerNorm(hidden_size, eps=eps)
         self.decoder = nn.TransformerDecoder(decoder_layer,
-                                             num_layers=num_decoder_layers)
+                                             num_layers=num_layers,
+                                             norm=norm)
 
     def forward(self,
                 src: torch.Tensor,
