8cddcdb56c389ecd5c544e8a646db163fd633d78
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-08-15 17:36:41 +0900

    Implement BertSmTokenizer

--- src/bertsum/preprocessing/tokenization.py
+++ src/bertsum/preprocessing/tokenization.py
@@ -1,13 +1,47 @@
-from typing import List
+from functools import reduce
+from typing import List, Optional, Union
 
-from transformers import AutoTokenizer
+from transformers import BertTokenizer
 
 
-class Tokenizer:
-    def __init__(self, model_type: str):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_type)
+class BertSumTokenizer(BertTokenizer):
+    def build_inputs_with_special_tokens(self,
+                                         token_ids_0: List[int],
+                                         token_ids_1: Optional[List[int]] = None) -> List[int]:
+        cls = [self.cls_token_id]
+        sep = [self.sep_token_id]
 
-    def __call__(self, text: str) -> List[int]:
-        tokens = self.tokenizer.tokenize(text)
-        token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-        return token_ids
+        if token_ids_1 is None:
+            return cls + token_ids_0 + sep
+
+        if self.additional_special_tokens_ids:
+            insert_token_ids = self.additional_special_tokens_ids
+        else:
+            insert_token_ids = sep + cls
+
+        return cls \
+            + token_ids_0 \
+            + insert_token_ids \
+            + token_ids_1 \
+            + sep
+
+    def create_token_type_ids_from_sequences(self,
+                                             token_ids_0: List[int],
+                                             token_ids_1: Optional[List[int]] = None) -> List[int]:
+        cls = [self.cls_token_id]
+        sep = [self.sep_token_id]
+
+        if token_ids_1 is None:
+            return len(cls + token_ids_0 + sep) * [0]
+
+        if self.additional_special_tokens_ids:
+            insert_token_ids = self.additional_special_tokens_ids
+        else:
+            insert_token_ids = sep + cls
+
+        i = len(insert_token_ids) // 2
+        former_insert_token_ids = insert_token_ids[:-i]
+        latter_insert_token_ids = insert_token_ids[-i:]
+
+        return len(cls + token_ids_0 + former_insert_token_ids) * [0] \
+            + len(latter_insert_token_ids + token_ids_1 + sep) * [1]
