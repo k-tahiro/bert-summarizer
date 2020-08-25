c84bc21d23074e11db7599b16d990072f17de84a
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-08-25 19:41:50 +0900

    temp commit

--- src/bertsum/summarizer/bertsum.py
+++ src/bertsum/summarizer/bertsum.py
@@ -1,11 +1,110 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 import torch
 from torch.utils.data import DataLoader
 
 from ..datasets.bertsum import BertSumDataset, BertSumExtDataset, BertSumAbsDataset
 from ..models.bertsum import BertSum, BertSumExt, BertSumAbs
+from ..preprocessing.tokenization import BertSumTokenizer
 from ..utils.tensor import tile
+
+
+class BeamSearch:
+    def __init__(self,
+                 tokenizer: BertSumTokenizer,
+                 batch_size: int,
+                 bos_token_id: int,
+                 eos_token_id: int,
+                 beam_size: int = 5,
+                 device: Optional[torch.device] = None,
+                 alpha: float = .6,
+                 block_trigram: bool = True):
+        self.beam_size = beam_size
+        self.tokenizer = tokenizer
+        self.alpha = alpha
+        self.block_trigram = block_trigram
+        self.bos_token_id = bos_token_id
+        self.eos_token_id = eos_token_id
+        self.device = device
+        self.batch_size = batch_size
+        self.beam_batch_size = self.beam_size * batch_size
+        self.batch_offset = torch.arange(batch_size,
+                                         dtype=torch.long,
+                                         device=self.device)
+        self.beam_offset = torch.arange(0,
+                                        self.beam_batch_size,
+                                        step=self.beam_size,
+                                        dtype=torch.long,
+                                        device=self.device)
+
+    def initialize(self):
+        self.alive_seq = torch.full([self.beam_batch_size, 1],
+                                    self.bos_token_id,
+                                    dtype=torch.long,
+                                    device=self.device)
+        self.topk_log_probs = torch.full([self.beam_batch_size, 1],
+                                         float('-inf'),
+                                         device=self.device)
+        self.topk_log_probs[self.beam_offset] = 0.0
+
+    def get_topk_ids(self, log_probs: torch.Tensor) -> torch.Tensor:
+        vocab_size = log_probs.size(-1)
+
+        # Multiply probs by the beam probability.
+        log_probs += self.topk_log_probs
+
+        # Flatten probs into a list of possibilities.
+        curr_scores = log_probs / self.length_penalty
+
+        if self.block_trigram:
+            curr_scores = self._block_trigram(curr_scores)
+
+        curr_scores = curr_scores.reshape(-1,
+                                          self.beam_size * vocab_size)
+        topk_scores, topk_ids = curr_scores.topk(self.beam_size,
+                                                 dim=-1)
+
+        # Recover log probs.
+        self.topk_log_probs = topk_scores * self.length_penalty
+
+        # Resolve beam origin and true word ids.
+        topk_beam_index = topk_ids.floor_divide(vocab_size)
+        topk_ids = topk_ids.fmod(vocab_size)
+
+        # Map beam_index to batch_index in the flat representation.
+        batch_index = topk_beam_index + \
+            self.beam_offset[:topk_beam_index.size(0)].unsqueeze(1)
+        select_indices = batch_index.view(-1)
+
+        # Append last prediction.
+        alive_seq = self.alive_seq.index_select(0, select_indices)
+        self.alive_seq = torch.cat([alive_seq, topk_ids.view(-1, 1)], -1)
+
+        return topk_ids
+
+    @property
+    def step(self):
+        return self.alive_seq.size(-1)
+
+    @property
+    def length_penalty(self):
+        return ((5.0 + (self.step + 1)) / 6.0) ** self.alpha
+
+    def _block_trigram(self, curr_scores: torch.Tensor) -> torch.Tensor:
+        convert_ids_to_tokens = self.tokenizer.tokenizer.convert_ids_to_tokens
+        cur_len = self.alive_seq.size(1)
+        if cur_len > 3:
+            for i, token_ids in enumerate(self.alive_seq.tolist()):
+                words = convert_ids_to_tokens(token_ids)
+                words = ' '.join(words).replace(' ##', '').split()
+                if len(words) <= 3:
+                    continue
+                trigrams = [(words[j-2], words[j-1], words[j])
+                            for j in range(2, len(words))]
+                trigram = trigrams[-1]
+                if trigram in trigrams[:-1]:
+                    curr_scores[i] = float('-inf')
+        return curr_scores
 
 
 class BertSumSummarizer:
@@ -22,10 +121,10 @@ class BertSumSummarizer:
         self.batch_size = batch_size
         self.device = device or model.device
 
-    def __call__(self, src: Union[str, List[str]]):
+    def __call__(self, src: Union[str, List[str]], *args, **kwargs):
         data_loader = self._create_data_loader(src)
         with torch.no_grad():
-            self._run(data_loader)
+            self._run(data_loader, *args, **kwargs)
 
     def _create_data_loader(self, src: Union[str, List[str]]) -> DataLoader:
         if isinstance(src, str):
@@ -49,119 +148,74 @@ class BertSumAbsSummarizer(BertSumSummarizer):
 
     def __init__(self,
                  model: BertSumAbs,
+                 tokenizer: BertSumTokenizer,
                  bos_token_id: int,
                  eos_token_id: int,
                  batch_size: Optional[int] = None,
-                 device: Optional[torch.device] = None,
-                 max_length: Optional[int] = None,
-                 min_length: int = 0,
-                 alpha: float = 1.,
-                 beam_size: int = 5):
+                 device: Optional[torch.device] = None):
         super(BertSumAbsSummarizer, self).__init__(model, batch_size, device)
+        self.tokenizer = tokenizer
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
-        self.beam_size = beam_size
-        self.max_length = max_length or self.model.encoder.config.max_position_embeddings
-        self.min_length = min_length
-        self.alpha = alpha
 
-    def _length_penalty(self, step: int):
-        return ((5.0 + (step + 1)) / 6.0) ** self.alpha
-
-    def _run(self, data_loader: DataLoader):
+    def _run(self,
+             data_loader: DataLoader,
+             max_length: Optional[int] = None,
+             min_length: int = 0,
+             alpha: float = .6,
+             beam_size: int = 5,
+             block_trigram: bool = True):
         batch_size = data_loader.batch_size
+        max_length = max_length or self.model.encoder.config.max_position_embeddings
+        beam_search = BeamSearch(self.tokenizer,
+                                 batch_size,
+                                 self.bos_token_id,
+                                 self.eos_token_id,
+                                 beam_size,
+                                 self.device,
+                                 alpha,
+                                 block_trigram)
+
         for src, _ in data_loader:
-            src_features, _ = self.model.encoder(**src)
+            beam_search.initialize()
+
+            memory, _ = self.model.encoder(**src)
+            memory = memory.permute(1, 0, 2).contiguous()
+            memory_key_padding_mask = src['attention_mask'] == 0
 
             # beamed batch features
-            src_features = tile(src_features,
-                                self.beam_size,
-                                dim=0)
-            batch_offset = torch.arange(batch_size,
-                                        dtype=torch.long,
-                                        device=self.device)
-            beam_offset = torch.arange(0,
-                                       batch_size * self.beam_size,
-                                       step=self.beam_size,
-                                       dtype=torch.long,
-                                       device=self.device)
-            alive_seq = torch.full([batch_size * self.beam_size, 1],
-                                   self.bos_token_id,
-                                   dtype=torch.long,
-                                   device=self.device)
-
-            # Give full probability to the first beam on the first step.
-            topk_log_probs = torch.tensor(
-                [0.0] + [float("-inf")] * (self.beam_size - 1),
-                device=self.device).repeat(batch_size)
+            memory = tile(memory, beam_search.beam_size, dim=1)
+            memory_key_padding_mask = tile(memory_key_padding_mask,
+                                           beam_search.beam_size,
+                                           dim=0)
 
             # Structure that holds finished hypotheses.
             hypotheses = [[] for _ in range(batch_size)]  # noqa: F812
 
-            results = {}
-            results["predictions"] = [[] for _ in range(batch_size)]  # noqa: F812
-            results["scores"] = [[] for _ in range(batch_size)]  # noqa: F812
-            results["gold_score"] = [0] * batch_size
-            results["batch"] = batch
-
-            for step in range(self.max_length):
-                decoder_input = alive_seq[:, -1].view(1, -1)
-
-                # Decoder forward.
-                decoder_input = decoder_input.transpose(0, 1)
-
-                dec_out, dec_states = self.model.decoder(decoder_input, src_features, dec_states,
-                                                         step=step)
+            for step in range(max_length):
+                tgt = self.model.embeddings(beam_search.alive_seq[:, -1])
+                tgt = self.model.pos_emb(tgt, step=step)
+                tgt = tgt.permute(1, 0, 2).contiguous()
+                dec_out = self.model.decoder(tgt, memory,
+                                             memory_key_padding_mask=memory_key_padding_mask)
 
                 # Generator forward.
-                log_probs = self.model.generator.forward(
-                    dec_out.transpose(0, 1).squeeze(0))
-                vocab_size = log_probs.size(-1)
+                dec_out = dec_out.permute(1, 0, 2).contiguous()
+                log_probs = self.model.generator(dec_out)
+                if step < min_length:
+                    log_probs[:, self.eos_token_id] = -1e20
+                topk_ids = beam_search.get_topk_ids(log_probs)
 
-                if step < self.min_length:
-                    log_probs[:, self.eos_token_id] = float('-inf')
-
-                # Multiply probs by the beam probability.
-                log_probs += topk_log_probs.view(-1).unsqueeze(1)
-
-                alpha = self.global_scorer.alpha
-                length_penalty = ((5.0 + (step + 1)) / 6.0) ** alpha
-
-                # Flatten probs into a list of possibilities.
-                curr_scores = log_probs / length_penalty
-
-                if(self.args.block_trigram):
-
-                curr_scores = curr_scores.reshape(-1, beam_size * vocab_size)
-                topk_scores, topk_ids = curr_scores.topk(beam_size, dim=-1)
-
-                # Recover log probs.
-                topk_log_probs = topk_scores * length_penalty
-
-                # Resolve beam origin and true word ids.
-                topk_beam_index = topk_ids.div(vocab_size)
-                topk_ids = topk_ids.fmod(vocab_size)
-
-                # Map beam_index to batch_index in the flat representation.
-                batch_index = (
-                    topk_beam_index
-                    + beam_offset[:topk_beam_index.size(0)].unsqueeze(1))
-                select_indices = batch_index.view(-1)
-
-                # Append last prediction.
-                alive_seq = torch.cat(
-                    [alive_seq.index_select(0, select_indices),
-                     topk_ids.view(-1, 1)], -1)
-
-                is_finished = topk_ids.eq(self.end_token)
+                is_finished = topk_ids.eq(self.eos_token_id)
                 if step + 1 == max_length:
-                    is_finished.fill_(1)
+                    is_finished.fill_(True)
                 # End condition is top beam is finished.
                 end_condition = is_finished[:, 0].eq(1)
                 # Save finished hypotheses.
                 if is_finished.any():
                     predictions = alive_seq.view(-1,
-                                                 beam_size, alive_seq.size(-1))
+                                                 self.beam_size,
+                                                 alive_seq.size(-1))
                     for i in range(is_finished.size(0)):
                         b = batch_offset[i]
                         if end_condition[i]:
@@ -196,150 +250,3 @@ class BertSumAbsSummarizer(BertSumSummarizer):
                 src_features = src_features.index_select(0, select_indices)
                 dec_states.map_batch_fn(
                     lambda state, dim: state.index_select(dim, select_indices))
-
-        def _block_trigram(self, alive_seq: torch.Tensor, curr_scores):
-            cur_len = alive_seq.size(1)
-            if(cur_len > 3):
-                for i in range(alive_seq.size(0)):
-                    fail = False
-                    words = [int(w) for w in alive_seq[i]]
-                    words = [self.vocab.convert_ids_to_tokens([w])[0]
-                             for w in words]
-                    words = ' '.join(words).replace(' ##', '').split()
-                    if(len(words) <= 3):
-                        continue
-                    trigrams = [(words[i-1], words[i], words[i+1])
-                                for i in range(1, len(words)-1)]
-                    trigram = tuple(trigrams[-1])
-                    if trigram in trigrams[:-1]:
-                        fail = True
-                    if fail:
-                        curr_scores[i] = -10e20
-            return curr_scores
-
-
-class DecoderState:
-    """Interface for grouping together the current state of a recurrent
-    decoder. In the simplest case just represents the hidden state of
-    the model.  But can also be used for implementing various forms of
-    input_feeding and non-recurrent models.
-
-    Modules need to implement this to utilize beam search decoding.
-    """
-
-    def detach(self):
-        """ Need to document this """
-        self.hidden = tuple([_.detach() for _ in self.hidden])
-        self.input_feed = self.input_feed.detach()
-
-    def beam_update(self, idx, positions, beam_size):
-        """ Need to document this """
-        for e in self._all:
-            sizes = e.size()
-            br = sizes[1]
-            if len(sizes) == 3:
-                sent_states = e.view(sizes[0], beam_size, br // beam_size,
-                                     sizes[2])[:, :, idx]
-            else:
-                sent_states = e.view(sizes[0], beam_size,
-                                     br // beam_size,
-                                     sizes[2],
-                                     sizes[3])[:, :, idx]
-
-            sent_states.data.copy_(
-                sent_states.data.index_select(1, positions))
-
-    def map_batch_fn(self, fn):
-        raise NotImplementedError()
-
-
-class TransformerDecoderState(DecoderState):
-    """ Transformer Decoder state base class """
-
-    def __init__(self, src):
-        """
-        Args:
-            src (FloatTensor): a sequence of source words tensors
-                    with optional feature tensors, of size (len x batch).
-        """
-        self.src = src
-        self.previous_input = None
-        self.previous_layer_inputs = None
-        self.cache = None
-
-    @property
-    def _all(self):
-        """
-        Contains attributes that need to be updated in self.beam_update().
-        """
-        if (self.previous_input is not None
-                and self.previous_layer_inputs is not None):
-            return (self.previous_input,
-                    self.previous_layer_inputs,
-                    self.src)
-        else:
-            return (self.src,)
-
-    def detach(self):
-        if self.previous_input is not None:
-            self.previous_input = self.previous_input.detach()
-        if self.previous_layer_inputs is not None:
-            self.previous_layer_inputs = self.previous_layer_inputs.detach()
-        self.src = self.src.detach()
-
-    def update_state(self, new_input, previous_layer_inputs):
-        state = TransformerDecoderState(self.src)
-        state.previous_input = new_input
-        state.previous_layer_inputs = previous_layer_inputs
-        return state
-
-    def _init_cache(self, memory_bank, num_layers):
-        self.cache = {}
-
-        for l in range(num_layers):
-            layer_cache = {
-                "memory_keys": None,
-                "memory_values": None
-            }
-            layer_cache["self_keys"] = None
-            layer_cache["self_values"] = None
-            self.cache["layer_{}".format(l)] = layer_cache
-
-    def repeat_beam_size_times(self, beam_size):
-        """ Repeat beam_size times along batch dimension. """
-        self.src = self.src.data.repeat(1, beam_size, 1)
-
-    def map_batch_fn(self, fn):
-        def _recursive_map(struct, batch_dim=0):
-            for k, v in struct.items():
-                if v is not None:
-                    if isinstance(v, dict):
-                        _recursive_map(v)
-                    else:
-                        struct[k] = fn(v, batch_dim)
-
-        self.src = fn(self.src, 0)
-        if self.cache is not None:
-            _recursive_map(self.cache)
-
-
-def tile(x, count, dim=0):
-    """
-    Tiles x on dimension dim count times.
-    """
-    perm = list(range(len(x.size())))
-    if dim != 0:
-        perm[0], perm[dim] = perm[dim], perm[0]
-        x = x.permute(perm).contiguous()
-    out_size = list(x.size())
-    out_size[0] *= count
-    batch = x.size(0)
-    x = x.view(batch, -1) \
-         .transpose(0, 1) \
-         .repeat(count, 1) \
-         .transpose(0, 1) \
-         .contiguous() \
-         .view(*out_size)
-    if dim != 0:
-        x = x.permute(perm).contiguous()
-    return x
