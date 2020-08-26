19936d9b8a134ff5f2f8db5319bb5756b1c240b1
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-08-26 19:31:42 +0900

    temp commit

--- src/bertsum/summarizer/bertsum.py
+++ src/bertsum/summarizer/bertsum.py
@@ -19,14 +19,14 @@ class BeamSearch:
                  device: Optional[torch.device] = None,
                  alpha: float = .6,
                  block_trigram: bool = True):
-        self.beam_size = beam_size
         self.tokenizer = tokenizer
-        self.alpha = alpha
-        self.block_trigram = block_trigram
+        self.batch_size = batch_size
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
+        self.beam_size = beam_size
+        self.alpha = alpha
+        self.block_trigram = block_trigram
         self.device = device
-        self.batch_size = batch_size
         self.beam_batch_size = self.beam_size * batch_size
         self.batch_offset = torch.arange(batch_size,
                                          dtype=torch.long,
@@ -37,17 +37,34 @@ class BeamSearch:
                                         dtype=torch.long,
                                         device=self.device)
 
+    @property
+    def seq_len(self):
+        return self.alive_seq.size(-1)
+
+    @property
+    def length_penalty(self):
+        return ((5.0 + self.seq_len) / 6.0)**self.alpha
+
+    @property
+    def length_penalty_for_eos(self):
+        return ((5.0 + self.seq_len - 1) / 6.0) ** self.alpha / self.length_penalty
+
     def initialize(self):
         self.alive_seq = torch.full([self.beam_batch_size, 1],
                                     self.bos_token_id,
                                     dtype=torch.long,
                                     device=self.device)
-        self.topk_log_probs = torch.full([self.beam_batch_size, 1],
+        self.topk_log_probs = torch.full((self.beam_batch_size,),
                                          float('-inf'),
                                          device=self.device)
         self.topk_log_probs[self.beam_offset] = 0.0
+        self.hypothesis = [[] for _ in range(self.batch_size)]
 
-    def get_topk_ids(self, log_probs: torch.Tensor) -> torch.Tensor:
+    def run(self, log_probs: torch.Tensor):
+        self.update_beam(log_probs)
+        self.finalize()
+
+    def update_beam(self, log_probs: torch.Tensor):
         vocab_size = log_probs.size(-1)
 
         # Multiply probs by the beam probability.
@@ -55,6 +72,7 @@ class BeamSearch:
 
         # Flatten probs into a list of possibilities.
         curr_scores = log_probs / self.length_penalty
+        curr_scores[:, self.eos_token_id] /= self.length_penalty_for_eos
 
         if self.block_trigram:
             curr_scores = self._block_trigram(curr_scores)
@@ -64,12 +82,16 @@ class BeamSearch:
         topk_scores, topk_ids = curr_scores.topk(self.beam_size,
                                                  dim=-1)
 
-        # Recover log probs.
-        self.topk_log_probs = topk_scores * self.length_penalty
-
         # Resolve beam origin and true word ids.
         topk_beam_index = topk_ids.floor_divide(vocab_size)
         topk_ids = topk_ids.fmod(vocab_size)
+
+        for i, token_id in enumerate(topk_ids):
+            if token_id == self.eos_token_id:
+                topk_scores[i] *= self.length_penalty_for_eos
+
+        # Recover log probs.
+        self.topk_log_probs = topk_scores * self.length_penalty
 
         # Map beam_index to batch_index in the flat representation.
         batch_index = topk_beam_index + \
@@ -80,15 +102,50 @@ class BeamSearch:
         alive_seq = self.alive_seq.index_select(0, select_indices)
         self.alive_seq = torch.cat([alive_seq, topk_ids.view(-1, 1)], -1)
 
-        return topk_ids
+    def finalize(self, is_final: bool = False):
+        predictions = self.alive_seq.view(-1,
+                                          self.beam_size,
+                                          self.alive_seq.size(-1))
 
-    @property
-    def step(self):
-        return self.alive_seq.size(-1)
+        is_finished = predictions[:, :, -1].eq(self.eos_token_id)
+        if is_final:
+            is_finished.fill_(True)
 
-    @property
-    def length_penalty(self):
-        return ((5.0 + (self.step + 1)) / 6.0) ** self.alpha
+        # End condition is top beam is finished.
+        end_condition = is_finished[:, 0].eq(True)
+
+        # Save finished hypotheses.
+        if is_finished.any():
+            for i in range(is_finished.size(0)):
+                b = self.batch_offset[i]
+                if end_condition[i]:
+                    is_finished[i].fill_(True)
+                finished_hyp = is_finished[i].nonzero().view(-1)
+
+                # Store finished hypotheses for this batch.
+                for j in finished_hyp:
+                    hypotheses[b].append((topk_scores[i, j],
+                                          predictions[i, j, 1:]))
+
+                # If the batch reached the end, save the n_best hypotheses.
+                if end_condition[i]:
+                    best_hyp = sorted(
+                        hypotheses[b], key=lambda x: x[0], reverse=True)
+                    score, pred = best_hyp[0]
+
+                    results["scores"][b].append(score)
+                    results["predictions"][b].append(pred)
+            non_finished = end_condition.eq(0).nonzero().view(-1)
+            # If all sentences are translated, no need to go further.
+            if len(non_finished) == 0:
+                break
+            # Remove finished batches for the next step.
+            topk_log_probs = topk_log_probs.index_select(
+                0, non_finished)
+            batch_index = batch_index.index_select(0, non_finished)
+            batch_offset = batch_offset.index_select(0, non_finished)
+            alive_seq = predictions.index_select(0, non_finished) \
+                .view(-1, alive_seq.size(-1))
 
     def _block_trigram(self, curr_scores: torch.Tensor) -> torch.Tensor:
         convert_ids_to_tokens = self.tokenizer.tokenizer.convert_ids_to_tokens
@@ -193,24 +250,20 @@ class BertSumAbsSummarizer(BertSumSummarizer):
             hypotheses = [[] for _ in range(batch_size)]  # noqa: F812
 
             for step in range(max_length):
-                tgt = self.model.embeddings(beam_search.alive_seq[:, -1])
-                tgt = self.model.pos_emb(tgt, step=step)
-                tgt = tgt.permute(1, 0, 2).contiguous()
-                dec_out = self.model.decoder(tgt, memory,
-                                             memory_key_padding_mask=memory_key_padding_mask)
-
-                # Generator forward.
-                dec_out = dec_out.permute(1, 0, 2).contiguous()
-                log_probs = self.model.generator(dec_out)
-                if step < min_length:
-                    log_probs[:, self.eos_token_id] = -1e20
+                log_probs = self._calc_token_probs(beam_search.alive_seq,
+                                                   step,
+                                                   memory,
+                                                   memory_key_padding_mask,
+                                                   min_length)
                 topk_ids = beam_search.get_topk_ids(log_probs)
 
                 is_finished = topk_ids.eq(self.eos_token_id)
                 if step + 1 == max_length:
                     is_finished.fill_(True)
+
                 # End condition is top beam is finished.
-                end_condition = is_finished[:, 0].eq(1)
+                end_condition = is_finished[:, 0].eq(True)
+
                 # Save finished hypotheses.
                 if is_finished.any():
                     predictions = alive_seq.view(-1,
@@ -250,3 +303,24 @@ class BertSumAbsSummarizer(BertSumSummarizer):
                 src_features = src_features.index_select(0, select_indices)
                 dec_states.map_batch_fn(
                     lambda state, dim: state.index_select(dim, select_indices))
+
+    def _calc_token_probs(self,
+                          alive_seq: torch.Tensor,
+                          step: int,
+                          memory: torch.Tensor,
+                          memory_key_padding_mask: torch.Tensor,
+                          min_length: int) -> torch.Tensor:
+        # decode
+        tgt = self.model.embeddings(alive_seq)
+        tgt = self.model.pos_emb(tgt, step=step)
+        tgt = tgt.permute(1, 0, 2).contiguous()
+        dec_out = self.model.decoder(tgt, memory,
+                                     memory_key_padding_mask=memory_key_padding_mask)
+
+        # Generator forward.
+        dec_out = dec_out.permute(1, 0, 2).contiguous()
+        log_probs = self.model.generator(dec_out)
+        if step < min_length:
+            log_probs[:, self.eos_token_id] = float('-inf')
+
+        return log_probs
