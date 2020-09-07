01d343654c2211665b147fc1e021ec7bbe3f2d53
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-09-07 10:42:43 +0900

    Fix arg name

--- src/bertsum/config/bertsum.py
+++ src/bertsum/config/bertsum.py
@@ -6,29 +6,28 @@ from transformers import BertConfig, BertTokenizer, EncoderDecoderConfig
 class BertSumExtConfig(BertConfig):
     def __init__(
         self,
-        bertsum_pretrained_model_name_or_path: str = 'bert-base-uncased',
+        base_model_name_or_path: str = 'bert-base-uncased',
         **kwargs
     ):
-        config = BertConfig.from_pretrained(bertsum_pretrained_model_name_or_path) \
+        config = BertConfig.from_pretrained(base_model_name_or_path) \
                            .to_dict()
         config.update(kwargs)
 
         super().__init__(**config)
 
-        tokenizer = BertTokenizer.from_pretrained(
-            bertsum_pretrained_model_name_or_path)
+        tokenizer = BertTokenizer.from_pretrained(base_model_name_or_path)
 
-        self.bertsum_pretrained_model_name_or_path = bertsum_pretrained_model_name_or_path
+        self.base_model_name_or_path = base_model_name_or_path
         self.cls_token_id = tokenizer.cls_token_id
 
 
 class BertSumAbsConfig(EncoderDecoderConfig):
     def __init__(
         self,
-        bertsum_pretrained_model_name_or_path: str = 'bert-base-uncased',
+        base_model_name_or_path: str = 'bert-base-uncased',
         **kwargs
     ):
-        encoder_config = BertConfig.from_pretrained(bertsum_pretrained_model_name_or_path) \
+        encoder_config = BertConfig.from_pretrained(base_model_name_or_path) \
                                    .to_dict()
         decoder_config = deepcopy(encoder_config)
         decoder_config.update(kwargs)
@@ -36,4 +35,4 @@ class BertSumAbsConfig(EncoderDecoderConfig):
         decoder_config['add_cross_attention'] = True
 
         super().__init__(encoder=encoder_config, decoder=decoder_config)
-        self.bertsum_pretrained_model_name_or_path = bertsum_pretrained_model_name_or_path
+        self.base_model_name_or_path = base_model_name_or_path
