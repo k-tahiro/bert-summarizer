fdcce6ec8d5885fff770321688e3f83208f27273
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-09-16 15:38:47 +0900

    Create Mixin class

--- src/bertsum/data/tokenizer.py
+++ src/bertsum/data/tokenizer.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Union
 from transformers import AutoTokenizer, BertTokenizer, BertJapaneseTokenizer
 
 
-class BertSumTokenizer(BertTokenizer):
+class BertSumMixin:
     def build_inputs_with_special_tokens(
         self,
         token_ids_0: List[int],
@@ -51,52 +51,12 @@ class BertSumTokenizer(BertTokenizer):
         return sent_sep_token_ids
 
 
-class BertSumJapaneseTokenizer(BertJapaneseTokenizer):
-    def build_inputs_with_special_tokens(
-        self,
-        token_ids_0: List[int],
-        token_ids_1: Optional[List[int]] = None
-    ) -> List[int]:
-        cls = [self.cls_token_id]
-        sep = [self.sep_token_id]
+class BertSumTokenizer(BertSumMixin, BertTokenizer):
+    pass
 
-        if token_ids_1 is None:
-            return cls + token_ids_0 + sep
 
-        sent_sep_token_ids = self.get_sent_sep_token_ids()
-
-        return cls \
-            + token_ids_0 \
-            + sent_sep_token_ids \
-            + token_ids_1 \
-            + sep
-
-    def create_token_type_ids_from_sequences(
-        self,
-        token_ids_0: List[int],
-        token_ids_1: Optional[List[int]] = None
-    ) -> List[int]:
-        cls = [self.cls_token_id]
-        sep = [self.sep_token_id]
-
-        if token_ids_1 is None:
-            return len(cls + token_ids_0 + sep) * [0]
-
-        sent_sep_token_ids = self.get_sent_sep_token_ids()
-        i = sum(divmod(len(sent_sep_token_ids), 2))
-        former_sent_sep_token_ids = sent_sep_token_ids[:i]
-        latter_sent_sep_token_ids = sent_sep_token_ids[i:]
-
-        return len(cls + token_ids_0 + former_sent_sep_token_ids) * [0] \
-            + len(latter_sent_sep_token_ids + token_ids_1 + sep) * [1]
-
-    def get_sent_sep_token_ids(self) -> List[int]:
-        if self.additional_special_tokens_ids:
-            sent_sep_token_ids = self.additional_special_tokens_ids
-        else:
-            sent_sep_token_ids = [self.sep_token_id, self.cls_token_id]
-
-        return sent_sep_token_ids
+class BertSumJapaneseTokenizer(BertSumMixin, BertJapaneseTokenizer):
+    pass
 
 
 class AutoBertSumTokenizer(AutoTokenizer):
@@ -107,6 +67,6 @@ class AutoBertSumTokenizer(AutoTokenizer):
         *inputs,
         **kwargs
     ) -> Union[BertSumTokenizer, BertSumJapaneseTokenizer]:
-        if "bert-base-japanese" in str(pretrained_model_name_or_path):
+        if 'bert-base-japanese' in str(pretrained_model_name_or_path):
             return BertSumJapaneseTokenizer.from_pretrained(pretrained_model_name_or_path, *inputs, **kwargs)
         return BertSumTokenizer.from_pretrained(pretrained_model_name_or_path, *inputs, **kwargs)
