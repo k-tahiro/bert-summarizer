6d403d6c8a09acc2fea0ece6082a11b13b5e1185
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-09-18 11:32:10 +0900

    Add 1

--- src/bert_summarizer/trainers/lr_lambda.py
+++ src/bert_summarizer/trainers/lr_lambda.py
@@ -9,6 +9,7 @@ def linear(num_training_steps: int, num_warmup_steps: int, current_step: int):
 
 
 def linear_then_invsqrt(num_warmup_steps: int, current_step: int):
+    current_step += 1
     return min(
         current_step ** -.5,
         current_step * num_warmup_steps ** -1.5
