c3d93ed7cb2bd1603fa47d77681678be9f63d818
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-12-24 18:47:43 +0900

    Add some tests

--- tests/models/test_bertsum.py
+++ tests/models/test_bertsum.py
@@ -17,12 +17,21 @@ class TestBertSumAbsDecoder:
     def model(self, config):
         return BertSumAbsDecoder(config.decoder)
 
-    def test_network_structure(self, config, model):
-        pass
+    def test_get_input_embeddings(self, model):
+        model.decoder.embeddings.make_embedding.emb_luts[0] = None
+        assert model.get_input_embeddings() is None
+
+    def test_set_input_embeddings(self, model):
+        model.set_input_embeddings(None)
+        assert model.decoder.embeddings.make_embedding.emb_luts[0] \
+            is None
 
     def test_embeddings_weight(self, model):
         assert id(model.get_input_embeddings().weight) \
             == id(model.get_output_embeddings().weight)
+
+    def test_network_structure(self, config, model):
+        pass
 
 
 class TestBertSumAbs:
