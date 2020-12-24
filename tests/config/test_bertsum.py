9da89f6c42ae00b5d885e41c6bd09510a3103dbb
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-12-24 18:26:16 +0900

    Add custom config test

--- tests/config/test_bertsum.py
+++ tests/config/test_bertsum.py
@@ -37,3 +37,21 @@ class TestBertSumAbsConfig:
         assert config.encoder_model_name_or_path == default_encoder_model_name_or_path
         assert config.encoder.to_dict() == default_encoder_config
         assert config.decoder.to_dict() == default_decoder_config
+
+    @pytest.mark.parametrize('kwargs', [
+        dict(
+            num_hidden_layers=6,
+            num_attention_heads=8,
+            intermediate_size=2048,
+            hidden_act='gelu',
+            hidden_dropout_prob=0.2,
+            attention_probs_dropout_prob=0.2,
+            layer_norm_eps=1e-6,
+        ),
+    ])
+    def test_custom_config(self, kwargs):
+        config = BertSumAbsConfig(**kwargs)
+        decoder_config = config.decoder.to_dict()
+        for k, v in kwargs.items():
+            assert k in decoder_config
+            assert decoder_config[k] == v
