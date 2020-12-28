f30266ec9a64e429f01a00367a6fbab797bc272b
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-12-28 12:58:20 +0900

    Fix unittest

--- tests/config/test_bertsum.py
+++ tests/config/test_bertsum.py
@@ -49,11 +49,11 @@ class TestBertSumExtConfig:
 
         assert config == default_bert_config.to_dict()
         assert base_model_name_or_path == default_model_name_or_path
-        assert encoder_config.to_dict() == default_ext_encoder_config.to_dict()
+        assert encoder_config == default_ext_encoder_config.to_dict()
 
     def test_custom_config(self, default_bert_config):
         config = BertSumExtConfig(encoder=default_bert_config)
-        assert config.encoder.to_dict() == default_bert_config.to_dict()
+        assert config.encoder == default_bert_config
 
 
 class TestBertSumAbsConfig:
