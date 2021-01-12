779d5d6450d3ef157a7e8c0d71086c38fd0a4dd1
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-12 17:48:48 +0900

    Change arguments and default value

--- src/bert_summarizer/config/bertsum.py
+++ src/bert_summarizer/config/bertsum.py
@@ -12,30 +12,30 @@ class BertSumExtConfig(BertConfig):
     def __init__(
         self,
         base_model_name_or_path: str = 'bert-base-uncased',
-        encoder: Optional[BertConfig] = None,
-        encoder_num_hidden_layers: int = 12,
-        encoder_num_attention_heads: int = 12,
-        encoder_intermediate_size: int = 3072,
+        encoder_num_hidden_layers: int = 2,
+        encoder_num_attention_heads: int = 8,
+        encoder_intermediate_size: int = 2048,
         encoder_hidden_act: str = 'gelu',
-        encoder_attention_probs_dropout_prob: float = 0.1,
-        encoder_layer_norm_eps: float = 1e-12,
+        encoder_attention_probs_dropout_prob: float = 0.2,
+        encoder_layer_norm_eps: float = 1e-6,
+        encoder_initializer_range: float = 0.0,
+        encoder_xavier_initialization: bool = True,
         **kwargs
     ):
         config = BertConfig.from_pretrained(base_model_name_or_path) \
                            .to_dict()
         config.update(kwargs)
 
-        if encoder:
-            encoder_config = encoder
-        else:
-            encoder_config = BertConfig(
-                num_hidden_layers=encoder_num_hidden_layers,
-                num_attention_heads=encoder_num_attention_heads,
-                intermediate_size=encoder_intermediate_size,
-                hidden_act=encoder_hidden_act,
-                attention_probs_dropout_prob=encoder_attention_probs_dropout_prob,
-                layer_norm_eps=encoder_layer_norm_eps
-            )
+        encoder_config = BertConfig(
+            num_hidden_layers=encoder_num_hidden_layers,
+            num_attention_heads=encoder_num_attention_heads,
+            intermediate_size=encoder_intermediate_size,
+            hidden_act=encoder_hidden_act,
+            attention_probs_dropout_prob=encoder_attention_probs_dropout_prob,
+            layer_norm_eps=encoder_layer_norm_eps,
+            initializer_range=encoder_initializer_range,
+            xavier_initialization=encoder_xavier_initialization
+        )
         config.update(encoder=encoder_config)
 
         super().__init__(**config)
