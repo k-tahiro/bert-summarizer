8d319fdca648695ce7c5142a7d8db7561ada394d
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-03-26 11:14:44 +0900

    Add NGramPredixAllowedTokensFn class for constrained generation

--- src/bert_summarizer/utils/logits_process.py
+++ src/bert_summarizer/utils/logits_process.py
@@ -1,5 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import List
+
 import torch
 from transformers import LogitsProccessor
+
+
+class PrefixAllowedTokensFnBase(ABC):
+    @abstractmethod
+    def __call__(self, batch_id: int, inputs_ids: torch.Tensor) -> List[int]:
+        raise NotImplementedError
+
+
+class NGramPrefixAllowedTokensFn(PrefixAllowedTokensFnBase):
+    def __init__(self, vocab_size: int, ngrams: List[List[int]]):
+        self.all_tokens = list(range(vocab_size))
+        self.ngrams = ngrams
+
+    def __call__(self, batch_id: int, inputs_ids: torch.Tensor) -> List[int]:
+        return [
+            ngram[-1]
+            for ngram in self.ngrams
+            if self.match(ngram, inputs_ids)
+        ] or self.all_tokens
+
+    def match(self, ngram: List[int], inputs_ids: torch.Tensor) -> bool:
+        prefix = ngram[:-1]
+        actual = inputs_ids[-len(prefix):].tolist()
+        return actual == prefix
 
 
 class GlobalDistributionLogitsProcessor(LogitsProccessor):
